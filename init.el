14027562adcc858fb756fcf3542584e896370b5b
Pablo Acuna <pacuna@pm.me>
2021-01-02 17:16:30 -0600

    add initial emacs config

---
+++ init.el
@@ -0,0 +1,78 @@
+;; enable melpa if it isn't enabled
+(require 'package)
+(when (not (assoc "melpa" package-archives))
+  (setq package-archives (append '(("melpa" . "https://melpa.org/packages/")) package-archives)))
+(package-initialize)
+
+;; refresh package list if it is not already available
+(when (not package-archive-contents) (package-refresh-contents))
+
+;; install use-package if it isn't already installed
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+
+;;
+;;
+;;
+;; go-lsp basic configuration from: https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
+;;
+;;
+;;
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; Optional - provides fancier overlays.
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+;; Company mode is a standard completion package that works well with lsp-mode.
+(use-package company
+  :ensure t
+  :config
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
+;; Optional - provides snippet support.
+(use-package yasnippet
+  :ensure t
+  :commands yas-minor-mode
+  :hook (go-mode . yas-minor-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(doom-themes go-mode yasnippet company lsp-ui lsp-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package go-mode
+  :ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  (load-theme 'doom-molokai t)
+  (doom-themes-org-config))
+
+
+;; other conf
+(menu-bar-mode -1)
