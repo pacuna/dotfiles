59fdfe2a442ee9c4d95d18c47b2b8bae1dd18f1b
Pablo Acuna <pacuna@pm.me>
2021-01-25 22:25:52 -0600

    add init.el

--- init.el
+++ init.el
@@ -1,65 +1,14 @@
-;; enable melpa if it isn't enabled
 (require 'package)
-(when (not (assoc "melpa" package-archives))
-  (setq package-archives (append '(("melpa" . "https://melpa.org/packages/")) package-archives)))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; refresh package list if it is not already available
-(when (not package-archive-contents) (package-refresh-contents))
-
-;; install use-package if it isn't already installed
-(when (not (package-installed-p 'use-package))
-  (package-install 'use-package))
-
-;;
-;;
-;;
-;; go-lsp basic configuration from: https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
-;;
-;;
-;;
-(use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :hook (go-mode . lsp-deferred))
-
-;; Set up before-save hooks to format buffer and add/delete imports.
-;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
-
-;; Optional - provides fancier overlays.
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode)
-
-;; Company mode is a standard completion package that works well with lsp-mode.
-(use-package company
-  :ensure t
-  :config
-  ;; Optionally enable completion-as-you-type behavior.
-  (setq company-idle-delay 0)
-
-  (setq company-minimum-prefix-length 1)
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
-  (define-key company-active-map (kbd "C-n") #'company-select-next)
-  (define-key company-active-map (kbd "C-p") #'company-select-previous))
-
-;; Optional - provides snippet support.
-(use-package yasnippet
-  :ensure t
-  :commands yas-minor-mode
-  :hook (go-mode . yas-minor-mode))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(helm doom-themes go-mode yasnippet company lsp-ui lsp-mode use-package)))
+ '(package-selected-packages '(magit monokai-theme rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -67,29 +16,15 @@
  ;; If there is more than one, they won't work right.
  )
 
-(use-package go-mode
-  :ensure t)
-
-(use-package doom-themes
-  :ensure t
-  :config
-  (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t)
-  (load-theme 'doom-molokai t)
-  (doom-themes-org-config))
-
-(use-package helm
-  :ensure t
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ([f10] . helm-buffers-list)
-         ([S-f10] . helm-recentf))
-  :config
-  (helm-mode 1))
+;; rust-mode config
+(require 'rust-mode)
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
+(setq rust-format-on-save t)
+(define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
 
 
-
-;; other conf
+;; general conf
+(load-theme 'monokai t)
 (menu-bar-mode -1)
-(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
-(setq create-lockfiles nil)
+(setq make-backup-files nil)
