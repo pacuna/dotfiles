c31b38015b1f55c278555b9ff454df98c0ce7e2d
Pablo Acuna <pacuna@pm.me>
2026-03-30 22:00:31 -0500

    zsh: vi mode with mode indicator

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,5 +1,22 @@
+# ── vcs_info ──────────────────────────────────────────────────────────────────
+
 autoload -Uz vcs_info
 precmd() { vcs_info }
 zstyle ':vcs_info:git:*' formats ' (%b)'
 setopt PROMPT_SUBST
-PROMPT='%F{cyan}%~%f${vcs_info_msg_0_} %# '
+
+# ── Vi mode indicator ─────────────────────────────────────────────────────────
+
+function zle-line-init zle-keymap-select {
+  case $KEYMAP in
+    vicmd)      VI_MODE="%F{yellow}[N]%f" ;;
+    viins|main) VI_MODE="%F{green}[I]%f" ;;
+  esac
+  zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+# ── Prompt ────────────────────────────────────────────────────────────────────
+
+PROMPT='${VI_MODE} %F{cyan}%~%f${vcs_info_msg_0_} %# '
