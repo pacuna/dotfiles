f73b31d10724fe941eea9f9f3e67d4870d182861
Pablo Acuna <pacuna@pm.me>
2026-03-31 19:59:08 -0500

    nvim: initial config with lazy, LSP, treesitter

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,48 @@
+local map = vim.keymap.set
+
+-- Leader key
+vim.g.mapleader = " "
+
+-- Better escape
+map("i", "jk", "<Esc>")
+
+-- Move between windows
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- Move lines up/down in visual mode
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor centered when scrolling
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor centered when searching
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Don't yank on paste in visual mode
+map("v", "p", '"_dP')
+
+-- File tree
+map("n", "<leader>e", ":Neotree toggle<CR>")
+
+-- Telescope
+map("n", "<leader>ff", ":Telescope find_files<CR>")
+map("n", "<leader>fg", ":Telescope live_grep<CR>")
+map("n", "<leader>fb", ":Telescope buffers<CR>")
+
+-- LSP (also set up in plugins.lua on_attach, but here for reference)
+map("n", "gd", vim.lsp.buf.definition)
+map("n", "gr", vim.lsp.buf.references)
+map("n", "K",  vim.lsp.buf.hover)
+map("n", "<leader>rn", vim.lsp.buf.rename)
+map("n", "<leader>ca", vim.lsp.buf.code_action)
+map("n", "<leader>f",  vim.lsp.buf.format)
+
+-- Diagnostics
+map("n", "[d", vim.diagnostic.goto_prev)
+map("n", "]d", vim.diagnostic.goto_next)
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,37 @@
+local opt = vim.opt
+
+-- Line numbers
+opt.number         = true
+opt.relativenumber = true
+
+-- Indentation
+opt.tabstop        = 2
+opt.shiftwidth     = 2
+opt.expandtab      = true
+opt.smartindent    = true
+
+-- Search
+opt.ignorecase     = true
+opt.smartcase      = true
+opt.hlsearch       = false
+opt.incsearch      = true
+
+-- UI
+opt.termguicolors  = true
+opt.signcolumn     = "yes"   -- always show, prevents layout shifts
+opt.cursorline     = true
+opt.scrolloff      = 8       -- keep 8 lines above/below cursor
+opt.wrap           = false
+
+-- Splits
+opt.splitright     = true
+opt.splitbelow     = true
+
+-- Files
+opt.swapfile       = false
+opt.backup         = false
+opt.undofile       = true
+opt.undodir        = vim.fn.expand("~/.local/state/nvim/undo")
+
+-- Clipboard
+opt.clipboard      = "unnamedplus"  -- use system clipboard
